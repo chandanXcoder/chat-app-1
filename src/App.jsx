b53f2054b955d4a47000eaa1f86ec25b6c705300
chandan-xcoder <chaandanverma@gmail.com>
2025-09-27 15:10:14 +0530

    code in changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import Navbar from './component/Navbar'
-import Hero from './component/hero'
+import Hero from './component/Hero'
 import Footer from './component/Footer'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
      <Navbar />
